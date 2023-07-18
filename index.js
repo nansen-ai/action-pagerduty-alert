709f80702a78d8b4f0fe3dd000f54ed7dbdbdef3
Tarvi Pillessaar <tarvi.p@nansen.ai>
2023-07-18 14:34:00 +0300

    Add some additional input options
    also renamed dedup key

--- index.js
+++ index.js
@@ -4,46 +4,49 @@ const axios = require('axios');
 
 // Trigger the PagerDuty webhook with a given alert
 async function sendAlert(alert) {
-  try {
     const response = await axios.post('https://events.pagerduty.com/v2/enqueue', alert);
 
     if (response.status === 202) {
-      console.log(`Successfully sent PagerDuty alert. Response: ${JSON.stringify(response.data)}`);
+        console.log(`Successfully sent PagerDuty alert. Response: ${JSON.stringify(response.data)}`);
     } else {
-      core.setFailed(
-        `PagerDuty API returned status code ${response.status} - ${JSON.stringify(response.data)}`
-      );
+        core.setFailed(
+            `PagerDuty API returned status code ${response.status} - ${JSON.stringify(response.data)}`
+        );
     }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 // Run the action
-try {
-  const integrationKey = core.getInput('pagerduty-integration-key');
+(async () => {
+    try {
+        const integrationKey = core.getInput('pagerduty-integration-key');
+        const alertSummary = core.getInput('alert-summary') || `${context.repo.repo}: Error in "${context.workflow}" run by @${context.actor}`;
+        const alertSeverity = core.getInput('alert-severity') || 'critical';
+        const eventAction = core.getInput('alert-event-action') || 'trigger';
 
-  let alert = {
-    payload: {
-      summary: `${context.repo.repo}: Error in "${context.workflow}" run by @${context.actor}`,
-      timestamp: new Date().toISOString(),
-      source: 'GitHub Actions',
-      severity: 'critical',
-      custom_details: {
-        run_details: `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`,
-        related_commits: context.payload.commits
-          ? context.payload.commits.map((commit) => `${commit.message}: ${commit.url}`).join(', ')
-          : 'No related commits',
-      },
-    },
-    routing_key: integrationKey,
-    event_action: 'trigger',
-  };
-  const dedupKey = core.getInput('pagerduty-dedup-key');
-  if (dedupKey != '') {
-    alert.dedup_key = dedupKey;
-  }
-  sendAlert(alert);
-} catch (error) {
-  core.setFailed(error.message);
-}
+        let alert = {
+            routing_key: integrationKey,
+            event_action: eventAction,
+            payload: {
+                summary: alertSummary,
+                timestamp: new Date().toISOString(),
+                source: 'GitHub Actions',
+                severity: alertSeverity,
+                custom_details: {
+                    run_details: `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`,
+                    related_commits: context.payload.commits
+                        ? context.payload.commits.map((commit) => `${commit.message}: ${commit.url}`).join(', ')
+                        : 'No related commits',
+                },
+            },
+        };
+
+        const dedupKey = core.getInput('alert-dedup-key');
+        if (dedupKey != '') {
+            alert.dedup_key = dedupKey;
+        }
+
+        await sendAlert(alert);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
